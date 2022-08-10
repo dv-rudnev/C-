e00c7d2941c07139b90e0233a9268382ab8aa3a3
Dmitriy R <yurubu48@gmsil.com>
2022-08-11 02:35:13 +0300

    add Ex0005 If Else

---
+++ Ex0005_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string user = Console.ReadLine();
+
+if (user.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это ж МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(user);
+}
