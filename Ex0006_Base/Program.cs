7ba8f1e000b8fd7d21d5caa27ba572a0024407d6
Dmitriy R <yurubu48@gmsil.com>
2022-08-11 03:20:48 +0300

    add Ex0006 Base

---
+++ Ex0006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 4;
+int b = 3;
+int c = 8;
+int d = 7;
+int e = 1;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
