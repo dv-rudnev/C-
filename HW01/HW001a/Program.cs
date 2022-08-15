d38459d0db6a45411c80561e55cb419a32db1f2b
Dmitriy R <yurubu48@gmsil.com>
2022-08-15 20:16:03 +0300

    rename HW to HW01

---
+++ HW01/HW001a/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a + " больше " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine(b + " больше " + a);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
