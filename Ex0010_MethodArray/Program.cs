794670edb05798fa83a6fa928862c0e005a5b8d6
Dmitriy R <yurubu48@gmsil.com>
2022-08-15 20:19:09 +0300

    add Ex0010 MethodaArray and Ex0011 ArrayLibrary

---
+++ Ex0010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 4, 15, 26, 35, 38, 59, 60, 70, 74, 85, 92 };
+
+int n = array.Length;
+int find = 70;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
