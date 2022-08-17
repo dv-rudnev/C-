66b366a8ef238b1ce33251600c2191b506ae5e53
Dmitriy R <yurubu48@gmsil.com>
2022-08-18 01:46:13 +0300

    add HW branch and HomeWork03

---
+++ HW03/HW003c/Program.cs
@@ -0,0 +1,22 @@
+﻿// Таблица кубов ряда чисел
+
+int numb = ReadToInt("Введите число : ");
+
+for (int i = 1; i <= numb; i++)
+{ 
+    if (i != numb)
+    {
+        Console.Write($"{i * i * i}, ");
+    }
+    else
+    {
+        Console.Write($"{i * i * i}");
+    }
+}
+
+
+int ReadToInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
