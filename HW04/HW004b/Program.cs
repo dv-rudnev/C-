cb028599ecc638e76068804c7c3538cc7048c4e9
Dmitriy R <yurubu48@gmsil.com>
2022-08-21 16:28:43 +0300

    add HW04

---
+++ HW04/HW004b/Program.cs
@@ -0,0 +1,25 @@
+﻿// Сумма цифр в числа
+
+int numb = ReadToInt("Введите число: ");
+
+int gSum = SumOfDigits(numb);
+Console.WriteLine($"Сумма цифр числа {numb} равна {gSum}");
+
+
+int SumOfDigits(int temp)
+{
+    int sum = 0;
+    while (temp != 0)
+        {
+        sum += (temp % 10);
+        temp /= 10;
+        }
+    return sum;
+}
+
+
+int ReadToInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
