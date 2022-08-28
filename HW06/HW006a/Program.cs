26d03e929e3e1e56cf601eb9233dfc2edb8b6d5e
Dmitriy R <yurubu48@gmsil.com>
2022-08-28 06:35:58 +0300

    add HW06

---
+++ HW06/HW006a/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Пользователь вводит с клавиатуры M чисел через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223 -> 4
+*/
+
+Console.Write("Введите числа через зяпятую: ");
+string inputStr = Console.ReadLine();
+
+int count = 0;
+string tmpStr = string.Empty;
+
+for (int i = 0; i < inputStr.Length; i++)
+{
+    if (inputStr[i] != ',') tmpStr += inputStr[i];
+    else
+    {
+        if (Convert.ToInt32(tmpStr) > 0) count++;
+        tmpStr = string.Empty;
+    }
+}
+if (Convert.ToInt32(tmpStr) > 0) count++;
+
+Console.WriteLine($"Введено {count} чисел больше 0.");
