b88e6c92988712275f06e9719bcfa14690cf8eb6
Dmitriy R <yurubu48@gmsil.com>
2022-08-31 01:05:01 +0300

    add HW07

---
+++ HW07/HW007c/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+
+int[,] rndMatrix = new int[new Random().Next(4, 8), new Random().Next(4, 8)];
+
+FillMatrix2D(rndMatrix);
+PrintMatrix2D(rndMatrix);
+double[] avg = ColumnAverage(rndMatrix);
+
+for (int i = 0; i < avg.Length; i++)
+{
+    Console.Write($"{avg[i].ToString("0.00")} ");
+}
+
+
+void FillMatrix2D(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+double[] ColumnAverage(int[,] matrix)
+{
+    double[] average = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i,j];
+        }
+        average[j] = sum / matrix.GetLength(0);
+    }
+    return average;
+}
+
+void PrintMatrix2D(int[,] matrix)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Матрица размером {matrix.GetLength(0)}*{matrix.GetLength(1)}");
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
