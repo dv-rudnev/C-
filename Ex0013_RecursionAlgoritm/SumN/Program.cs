d0e6b0338269c0b2d00e8d32e69c7a4472ae3111
Dmitriy R <yurubu48@gmsil.com>
2022-09-08 22:03:33 +0300

    add Ex0013_RecursionAlgoritm

---
+++ Ex0013_RecursionAlgoritm/SumN/Program.cs
@@ -0,0 +1,18 @@
+﻿// Сумма чисел от 1 до N. Цикл и рекурсия.
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+
+
+Console.WriteLine(SumFor(8));
+Console.WriteLine(SumRec(8));
